2a1c8af886b3a425c588608a33c017f0a0ee818a
shotaPro <9688wpva@gmail.com>
2023-06-17 15:17:16 +0900

    first commit

---
+++ vue/src/views/Dashboard.vue
@@ -0,0 +1,8 @@
+<template>
+    <PageComponent title="Dashboard">Contents goes here</PageComponent>
+</template>
+
+<script setup>
+import PageComponent from "../components/PageComponent.vue";
+</script>
+
